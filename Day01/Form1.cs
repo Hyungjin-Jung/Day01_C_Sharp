8e95f9a9c89934c4b8bed0566e64b386ff6ce64a
Hyungjin <2hyungjin3@gmail.com>
2023-11-06 16:58:39 +0900

    Add project files.

---
+++ Day01/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // MessageBox.Show("안녕하세요우~");
+
+            // 텍스트 박스에 메시지 출력
+            textBox_print.Text = "이것은 \r\n 멀티라인 \r\n 텍스트박스 \r\n 인데요. ";
+        }
+    }
+}
