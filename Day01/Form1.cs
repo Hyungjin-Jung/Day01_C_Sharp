93b82784dfdfa3cc0d8290f0b465cfe63afd9d45
Hyungjin-Jung <120576967+Hyungjin-Jung@users.noreply.github.com>
2023-11-06 18:19:40 +0900

    Update Form1.cs

--- Day01/Form1.cs
+++ Day01/Form1.cs
@@ -19,7 +19,6 @@ namespace Day01
             // MessageBox.Show("안녕하세요우~");
             //  2번째
             // 텍스트 박스에 메시지 출력
-            // github에서 수정
             textBox_print.Text = "이것은 \r\n 멀티라인 \r\n 텍스트박스 \r\n 입니다!! ";
         }
     }
